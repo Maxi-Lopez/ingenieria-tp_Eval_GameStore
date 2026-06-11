326117d59a4c91bed0582d26172f1c5dd3706673
Tomas <t.alonsoarias@itecriocuarto.org.ar>
2026-06-11 11:29:57 -0300

    Se creo templates en videojuegos

--- usuarios/models.py
+++ usuarios/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import Group
 class UsuarioPersonalizado(AbstractUser):
     telefono = models.CharField(max_length = 20, null= True, blank=True)
     foto_perfil = models.ImageField(upload_to='users_fp', null=True, blank=True)
-    rol_usuario = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
+    rol_usuario = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, default=1)
     def __str__(self):
         return f"{self.first_name} - {self.last_name}"
     
