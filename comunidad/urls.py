c0fc3005e7f31dddd05e08f84b5613765887fac8
Tomas <t.alonsoarias@itecriocuarto.org.ar>
2026-06-12 11:12:09 -0300

    Se creo el crud de posteos en comunidad

--- comunidad/urls.py
+++ comunidad/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.posteos, name='posteos'),
+    path('eliminar_posteo/<int:id>/', views.eliminar_posteo, name='eliminar_posteo'),
+    path('editar_posteo/<int:id>/', views.editar_posteo, name='editar_posteo')
 ]
--- comunidad/views.py
+++ comunidad/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PosteoForm
 from .models import Posteo
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required
 def posteos(request):
     if request.method == 'POST':
         form = PosteoForm(request.POST, request.FILES)
@@ -13,6 +16,28 @@ def posteos(request):
             return redirect('posteos')
     else:
         form = PosteoForm()
-    posteos =Posteo.objects.filter(activo = True)
+    posteos =Posteo.objects.filter(activo = True).order_by('-id')
     
     return render(request, 'comunidad/posteos.html',{'form':form, 'posteos':posteos})
+
+@login_required
+def eliminar_posteo(request, id):
+    posteo = get_object_or_404(Posteo, id=id)
+    if request.method == 'POST':
+        posteo.activo = False
+        posteo.save()
+        return redirect('posteos')
+    return render(request, 'comunidad/eliminar_posteo.html', {'posteo':posteo})
+
+@login_required
+def editar_posteo(request,id):
+    posteo = get_object_or_404(Posteo, id=id)
+    if request.method == 'POST':
+        form = PosteoForm( request.POST, instance=posteo)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('posteos')
+    else:
+        form = PosteoForm(instance=posteo)
+    return render(request, 'comunidad/editar_posteo.html',{'posteo':posteo, 'form':form})
